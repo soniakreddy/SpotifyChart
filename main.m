510b5ed5d54490ec79b4431cf7d2df5b93ddc893
Sonia Reddy Kolli <soniakreddy@gmail.com>
2016-02-27 20:04:05 -0800

    Added files via upload

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpotifyChart
+//
+//  Created by Sonia Reddy Kolli on 02/26/16.
+//  Copyright (c) 2016 Sonia Reddy Kolli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
