2dcfdd4862bdfdf214f550778dd78c2822cf8893
Sonia Reddy Kolli <soniakreddy@gmail.com>
2016-02-27 20:03:44 -0800

    Added files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpotifyChart
+//
+//  Created by Sonia Reddy Kolli on 02/26/16.
+//  Copyright (c) 2016 Sonia Reddy Kolli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Defines.h
@@ -0,0 +1,34 @@
+//
+//  Defines.h
+//  SpotifyChart
+//
+//  Created by Sonia Reddy Kolli on 02/26/16.
+//  Copyright (c) 2016 Sonia Reddy Kolli. All rights reserved.
+//
+
+#ifndef Defines_h
+#define Defines_h
+
+#define KEY_ENTRIES @"entries"
+#define KEY_ITEMS @"items"
+#define KEY_CURRPOS @"current_position"
+#define KEY_PREVPOS @"previous_position"
+#define KEY_PLAYS @"plays"
+#define KEY_TRACK @"track"
+#define KEY_ID @"id"
+#define KEY_NAME @"name"
+#define KEY_PREVIEW_URL @"preview_url"
+#define KEY_TRACK_URL @"href"
+#define KEY_DURATION_MS @"duration_ms"
+#define KEY_ARTISTS @"artists"
+#define KEY_ARTIST_NAME @"name"
+#define KEY_ALBUM_IMAGE_URL @"url"
+#define KEY_ALBUM_IMAGES @"images"
+#define KEY_ALBUM_ALBUM @"album"
+#define KEY_ALBUM_URL @"url"
+
+
+
+
+
+#endif /* Defines_h */
