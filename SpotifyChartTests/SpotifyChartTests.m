e5b3d98147a5919df54ba559e24ad44f277b14c0
Sonia Reddy Kolli <soniakreddy@gmail.com>
2016-02-27 19:42:15 -0800

    Added files via upload

---
+++ SpotifyChartTests/SpotifyChartTests.m
@@ -0,0 +1,39 @@
+//
+//  SpotifyChartTests.m
+//  SpotifyChartTests
+//
+//  Created by Sonia Reddy Kolli on 02/26/16.
+//  Copyright (c) 2016 Sonia Reddy Kolli. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpotifyChartTests : XCTestCase
+
+@end
+
+@implementation SpotifyChartTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
